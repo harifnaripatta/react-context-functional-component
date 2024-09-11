5ca31f824780bfd3983d2332576e119a3770c0a4
Harif Narippatta <harifnarippatta@gmail.com>
2024-09-11 16:33:58 +0300

    bookform completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import BookContextProvider from "./Context/BookContext";
 import Navbar from "./Components/Navbar";
 import BookList from "./Components/BookList";
+import BookForm from "./Components/BookForm";
 
 const App = () => {
   return (
     <BookContextProvider>
       <Navbar />
+      <BookForm />
       <BookList />
     </BookContextProvider>
   );
--- src/Components/BookForm.jsx
+++ src/Components/BookForm.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { BookContext } from "../Context/BookContext";
 
 const BookForm = () => {
-  return <div>BookForm</div>;
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const { addBook } = useContext(BookContext);
+  const handleSubmit = (e) => {
+    e.preventDefault(), addBook(title, author), setTitle(""), setAuthor("");
+  };
+  return (
+    <form onSubmit={handleSubmit} className="form-container">
+      <input
+        type="text"
+        placeholder="Enter Book Title"
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Enter Author Name"
+        onChange={(e) => setAuthor(e.target.value)}
+      />
+      <input type="submit" />
+    </form>
+  );
 };
 
 export default BookForm;
